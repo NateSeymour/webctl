0f67e8cc8eba2378fd93cef57e3ec9d369442e35
Nathan Seymour <nathan@seymour.global>
2025-09-08 19:20:36 -0400

    feat(server): full jwt verification

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 #include <vector>
 #include <mutex>
 #include <boost/beast/core.hpp>
-#include <boost/beast/core/detail/base64.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
 #include <boost/asio/strand.hpp>
@@ -22,14 +21,18 @@
 #include <boost/json.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/param_build.h>
+#include <cppcodec/base64_url_unpadded.hpp>
 
 namespace beast = boost::beast;
-namespace base64 = beast::detail::base64;
 namespace http = beast::http;
 namespace asio = boost::asio;
 namespace ssl = asio::ssl;
 namespace json = boost::json;
 using tcp = asio::ip::tcp;
+using base64 = cppcodec::base64_url_unpadded;
 
 /*
  * Helper for std::visit provided by Andreas Fertig.
@@ -403,20 +406,49 @@ RestProvider<WebCtlContext> web_ctl_rest_provider{
             return res_unauthorized;
         }
 
-        std::vector<char> buffer(base64::decoded_size(token.size()), '\0');
+        auto header_raw = base64::decode(token_parts[0]);
+        auto header = json::parse(std::string_view{(char*)header_raw.data(), header_raw.size()});
 
-        auto [header_size, header_read] = base64::decode(buffer.data(), token_parts[0].c_str(), token_parts[0].size());
-        auto header = json::parse(std::string_view{buffer.data(), header_size});
+        auto claims_raw = base64::decode(token_parts[1]);
+        auto claims = json::parse(std::string_view{(char*)claims_raw.data(), claims_raw.size()});
 
-        auto [claims_size, claims_read] = base64::decode(buffer.data(), token_parts[1].c_str(), token_parts[1].size());
-        auto claims = json::parse(std::string_view{buffer.data(), claims_size});
-
-        auto sig = token_parts[2];
+        std::string payload = token_parts[0] + "." + token_parts[1];
+        auto sig = base64::decode(token_parts[2]);
 
         auto key = ctx.jwks_provider.GetKeyById(header.as_object()["kid"].as_string().data());
         if (!key)
         {
             return res_unauthorized;
+        }
+
+        // Build key
+        EVP_PKEY_CTX *evp_ctx = EVP_PKEY_CTX_new_from_name(nullptr, "RSA", nullptr);
+        EVP_PKEY *pkey = nullptr;
+
+        std::string e_base64 = key.value().as_object()["e"].as_string().data();
+        std::string n_base64 = key.value().as_object()["n"].as_string().data();
+
+        auto e = base64::decode(e_base64);
+        auto n = base64::decode(n_base64);
+
+        OSSL_PARAM_BLD *param_bld = OSSL_PARAM_BLD_new();
+
+        BIGNUM *e_bn = BN_bin2bn(e.data(), static_cast<int>(e.size()), nullptr);
+        BIGNUM *n_bn = BN_bin2bn(n.data(), static_cast<int>(n.size()), nullptr);
+
+        OSSL_PARAM_BLD_push_BN(param_bld, "n", n_bn);
+        OSSL_PARAM_BLD_push_BN(param_bld, "e", e_bn);
+
+        OSSL_PARAM *params = OSSL_PARAM_BLD_to_param(param_bld);
+
+        if (auto err = EVP_PKEY_fromdata_init(evp_ctx); err <= 0)
+        {
+            std::cerr << ERR_error_string(err, nullptr) << std::endl;
+        }
+
+        if (auto err = EVP_PKEY_fromdata(evp_ctx, &pkey, EVP_PKEY_PUBLIC_KEY, params); err <= 0)
+        {
+            std::cerr << ERR_error_string(err, nullptr) << std::endl;
         }
 
         // Verify signature
@@ -431,12 +463,18 @@ RestProvider<WebCtlContext> web_ctl_rest_provider{
 
         EVP_VerifyInit(md_ctx, md);
 
-        EVP_VerifyUpdate(md_ctx, token_parts[1].c_str(), token_parts[1].size());
-        bool valid = EVP_VerifyFinal(md_ctx, reinterpret_cast<unsigned char const *>(token_parts[2].c_str()), token_parts[2].size(), nullptr);
+        EVP_VerifyUpdate(md_ctx, payload.c_str(), payload.size());
+        int valid = EVP_VerifyFinal(md_ctx, sig.data(), sig.size(), pkey);
 
+        EVP_PKEY_CTX_free(evp_ctx);
+        EVP_PKEY_free(pkey);
+        BN_free(e_bn);
+        BN_free(n_bn);
+        OSSL_PARAM_BLD_free(param_bld);
+        OSSL_PARAM_free(params);
         EVP_MD_CTX_free(md_ctx);
 
-        if (!valid)
+        if (valid != 1)
         {
             return res_unauthorized;
         }
