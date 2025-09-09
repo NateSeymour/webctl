fbdeb74d9e0d3ae1cfb8efa52fca07d2936f3cb7
Nathan Seymour <nathan@seymour.global>
2025-09-09 11:04:20 -0400

    feat(server): systemd response json serialization

--- src/main.cpp
+++ src/main.cpp
@@ -379,11 +379,53 @@ namespace sdbus
         return msg;
     }
 
-    sdbus::Message &operator>>(sdbus::Message &msg, json::string &array) { return msg; }
+    sdbus::Message &operator>>(sdbus::Message &msg, json::value &item);
 
-    sdbus::Message &operator>>(sdbus::Message &msg, json::string &object) { return msg; }
+    /**
+     * Used for deserializing arrays _and structs_ for lack of better container to put them in.
+     */
+    sdbus::Message &operator>>(sdbus::Message &msg, json::array &array)
+    {
+        auto [type, contents] = msg.peekType();
 
-    sdbus::Message &operator>>(sdbus::Message &msg, json::string &string) { return msg; }
+        if(type == 'a')
+        {
+            msg.enterContainer(contents);
+        }
+        else
+        {
+            msg.enterStruct(contents);
+        }
+
+        while(!msg.isAtEnd(false))
+        {
+            json::value item;
+            msg >> item;
+            array.push_back(item);
+        }
+
+        if(type == 'a')
+        {
+            msg.exitContainer();
+        }
+        else
+        {
+            msg.exitStruct();
+        }
+
+        return msg;
+    }
+
+    sdbus::Message &operator>>(sdbus::Message &msg, json::object &object) { return msg; }
+
+    sdbus::Message &operator>>(sdbus::Message &msg, json::string &string) 
+    { 
+        std::string value;
+        msg >> value;
+        string = value;
+
+        return msg; 
+    }
 
     sdbus::Message &operator>>(sdbus::Message &msg, json::value &item)
     {
@@ -391,16 +433,104 @@ namespace sdbus
 
         switch (type)
         {
-            case 's':
+            case 'y': 
             {
-                msg >> item.as_string();
+                std::uint8_t value;
+                msg >> value;
+                item = value;
+                break;
+            }
+
+            case 'b': 
+            {
+                bool value;
+                msg >> value;
+                item = value;
+                break;
+            }
+
+            case 'n': 
+            {
+                std::int16_t value;
+                msg >> value;
+                item = value;
+                break;
+            }
+
+            case 'q': 
+            {
+                std::uint16_t value;
+                msg >> value;
+                item = value;
+                break;
+            }
+
+            case 'i': 
+            {
+                std::int32_t value;
+                msg >> value;
+                item = value;
+                break;
+            }
+
+            case 'u': 
+            {
+                std::uint32_t value;
+                msg >> value;
+                item = value;
+                break;
+            }
+
+            case 'x': 
+            {
+                std::int64_t value;
+                msg >> value;
+                item = value;
+                break;
+            }
+
+            case 't': 
+            {
+                std::uint64_t value;
+                msg >> value;
+                item = value;
+                break;
+            }
+
+            case 'd': 
+            {
+                double value;
+                msg >> value;
+                item = value;
                 break;
             }
 
             case 's':
             {
+                item = json::string{};
+                msg >> item.as_string();
+                break;
+            }
+
+            case 'o':
+            {
+                sdbus::ObjectPath value;
+                msg >> value;
+                item = value.c_str();
+                break;
+            }
+
+            case 'r':
+            case 'a':
+            {
+                item = json::array{};
                 msg >> item.as_array();
                 break;
+            }
+
+            default:
+            {
+                throw std::runtime_error("Unable to deserialize type into JSON!");
             }
         }
 
@@ -550,7 +680,7 @@ RestProvider<WebCtlContext> web_ctl_rest_provider{
         json::value reply_json;
         reply >> reply_json;
 
-        res.body() = reply_json;
+        res.body() = json::serialize(reply_json);
 
         return res;
     }},
@@ -560,7 +690,7 @@ int main(int argc, char const **argv)
 {
     // 0. Read and Initialize Config
     // Load in ca certs
-    std::ifstream ca_certs{argv[1]};
+    std::ifstream ca_certs{"/etc/ssl/certs/ca-certificates.crt"};
     std::string ca_string{std::istreambuf_iterator<char>{ca_certs}, {}};
 
     // Setup asio context
